82abe10a6e1cd818236202d862263bb13fae5bda
viet <viet.tranhoang@powergatesoftware.com>
2018-01-04 23:49:37 +0700

    a

--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl,Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
@@ -8,17 +8,28 @@ import { FormGroup, FormControl,Validators } from '@angular/forms';
 export class ProductListComponent implements OnInit {
 
   constructor() { }
-  productForm : FormGroup;
+  productForm: FormGroup;
+  typeList = [
+    {id:1,name:"drink"},
+    {id:2,name:"fastfood"},
+    {id:3,name:"cake"},
+    {id:4,name:"acoholic drink"}
+  ]
   ngOnInit() {
     this.productForm = new FormGroup({
-      name: new FormControl('',[Validators.required]),
+      name: new FormControl('', [Validators.required]),
       type: new FormControl(),
       provider: new FormControl(),
       price: new FormControl(),
       newPrice: new FormControl(),
       description: new FormControl(),
-      status:new FormControl()
+      status: new FormControl()
     })
+    console.log(this.productForm.value)
+    this.setData();
   }
-
+  setData(){
+    this.productForm.value.description = 'test';
+    this.productForm.value.type =  {id:2,name:"fastfood"};
+  }
 }
